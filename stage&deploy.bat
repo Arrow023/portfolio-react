c820fa03f8ebb4867a368a85a2142024a7317f84
arrow023 <piyushchohan48@gmail.com>
2023-07-01 19:43:13 +0530

    Ver1.2

--- stage&deploy.bat
+++ stage&deploy.bat
@@ -4,12 +4,11 @@ prompt Arrow023$g
 cls
 echo "Starting NPM build...."
 START /B /WAIT cmd /c npm run-script build
-read "Press any key to continue...."
+color a
 echo "NPM build ready...."
 git add .
-echo "Enter commit Message:"
-read commitMsg
-git commit -m "$commitMsg"
+set /p commitMsg="Enter commit Message: "
+git commit -m "%commitMsg%"
 git push -u origin master
 echo "Source code pushed..."
 echo "Distribution copy started...."
@@ -17,6 +16,9 @@ START /B /WAIT cmd /c xcopy build ..\dist /i /e
 echo "Dist copy completed...."
 git add .
 echo "Enter commit message for dist :"
-read distMsg
-git commit -m "$distMsg"
+set /p distMsg="Enter commit Message: "
+git commit -m "%distMsg%"
 git push -u origin master
+echo "Netlify push completed......"
+pause
+exit
